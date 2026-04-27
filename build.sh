13c8fc42f5d72bdbda1e3f304e2e8e480b0214ba
Al-amin1911 <alaminogunbiyi@gmail.com>
2026-04-27 15:10:55 +0100

    5. basepath set and githud docs implementation

---
+++ build.sh
@@ -0,0 +1,1 @@
+python3 src/main.py "https://github.com/Al-amin1911/static-site-generator"
--- src/main.py
+++ src/main.py
@@ -1,10 +1,8 @@
 import logging
 from md_blocks import markdown_to_blocks, markdown_to_html_node
-import os, shutil, logging
-from dotenv import load_dotenv
+import os, shutil, logging, sys
 
 logger = logging.getLogger(__name__)
-load_dotenv()
 
 def copy_content(source_path:str, destination_path:str):
     # delete content of destination directory "public"
@@ -40,7 +38,7 @@ def extract_title(markdown):
         print(f"Error occured {e}")
 
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     try:
         with open(from_path) as f:
@@ -51,6 +49,8 @@ def generate_page(from_path, template_path, dest_path):
         title = extract_title(markdown)
         template = template.replace('{{ Title }}', title)
         template = template.replace('{{ Content }}', html_str)
+        template = template.replace('href="/', f'href="{basepath}')
+        template = template.replace('src="/', f'src="{basepath}')
         directory = os.path.dirname(dest_path)
         os.makedirs(directory, exist_ok=True)
         with open(dest_path, 'w') as f:
@@ -58,7 +58,7 @@ def generate_page(from_path, template_path, dest_path):
     except Exception as e:
         print(f"Error occured :{e}")
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     if os.path.exists(dir_path_content):
         try:
             for filename in os.listdir(dir_path_content):
@@ -68,10 +68,10 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
                     if os.path.isdir(content_path):
                         new_dest_dir = os.path.join(dest_dir_path, filename)
                         os.mkdir(new_dest_dir)
-                        generate_pages_recursive(content_path, template_path, new_dest_dir)
+                        generate_pages_recursive(content_path, template_path, new_dest_dir, basepath)
                     elif os.path.isfile(content_path):
                         if content_path.endswith(".md"):
-                            generate_page(content_path, template_path, dest_dir_path+"/index.html")
+                            generate_page(content_path, template_path, dest_dir_path+"/index.html", basepath)
                 except Exception as e:
                     print(f"Failed to generate file: {e}")
         except Exception as e:
@@ -81,11 +81,12 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
 
 def main():
     logging.basicConfig(filename='log_file', level=logging.INFO)
-    src = os.getenv('SOURCE_PATH')
-    dst = os.getenv("DEST_PATH")
-    copy_content(src, dst)
-    from_path, tmp_path, dst_path = os.getenv('FROM_PATH'), os.getenv('TEMPLATE_PATH'), os.getenv('DEST_PATH')
-    generate_pages_recursive(from_path, tmp_path, dst_path)
+    src = "../static"
+    dst = "../docs"
+    copy_content(src, dst)  
+    basepath =  sys.argv[1] if len(sys.argv) > 1 else "/"
+    from_path, tmp_path = "../content", "../template.html"
+    generate_pages_recursive(from_path, tmp_path, dst, basepath)
 
 
 
